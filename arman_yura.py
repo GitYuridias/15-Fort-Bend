c09c3f82d818daca586429620854501b479287e1
GitYuridias <yushakh@hotmail.com>
2022-01-24 01:39:58 +0400

    Progress Made

--- arman_yura.py
+++ arman_yura.py
@@ -18,9 +18,15 @@ driver.find_element(By.LINK_TEXT, "Criminal Case Records").click()
 # fill in the form (last name, first name, dob) and submit
 last = "Candler"
 first = "James"
+middle = ""
 birth = "07/23/1954"
-Internal_ID = "0436D707-7660-444E-84E4-3F7F89675B60"
 
+# last = "Williams"
+# first = "Willie"
+# middle = "Charles"
+# birth = ""
+
+Internal_ID = "0436D707-7660-444E-84E4-3F7F89675B60"
 
 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "LastName")))
 ln = driver.find_element(By.ID, "LastName")
@@ -30,6 +36,10 @@ ln.send_keys(last)
 fn = driver.find_element(By.ID, "FirstName")
 fn.clear()
 fn.send_keys(first)
+
+mid = driver.find_element(By.ID, "MiddleName")
+mid.clear()
+mid.send_keys(middle)
 
 dob = driver.find_element(By.ID, "DateOfBirth")
 dob.clear()
@@ -42,6 +52,7 @@ driver.implicitly_wait(5)
 results = {"primary": {}, "aliases": {}, 'info': {}}
 results["primary"]['First_Name'] = first
 results["primary"]['Last_Name'] = last
+results["primary"]['Middle_Name'] = middle
 results["primary"]['Date_of_Birth'] = birth[:-4] + birth[0:2] + birth[3:5]
 results["primary"]['State_Abbreviation'] = "TX"
 results["primary"]['Area'] = "Fort Bend"
@@ -57,7 +68,6 @@ if int(driver.find_element(By.XPATH, "/html/body/table[3]/tbody/tr[1]/td[2]/b").
 
     print('No cases for that person found')
     results["primary"]['Result_Not_Found'] = 'true'
-    # driver.quit()
 
 else:
     rows = driver.find_elements(By.XPATH, "/html/body/table[4]/tbody/tr")
@@ -71,14 +81,51 @@ else:
         results["info"][f"case_{i - 2}"]['State'] = driver.find_element(By.XPATH, "/html/body/table[5]/tbody/tr[3]/td").text.replace(" ", "").split(',')[1][:2]
         results["info"][f"case_{i - 2}"]['Zip'] = driver.find_element(By.XPATH, "/html/body/table[5]/tbody/tr[3]/td").text.replace(" ", "").split(',')[1][2:]
 
-        # FIXME dzel date i format@
-        results["info"][f"case_{i - 2}"]['CaseFileDate'] = driver.find_element(By.XPATH, "/html/body/table[3]/tbody/tr/td[3]/table/tbody/tr/td/table/tbody/tr[2]/td/b").text
+        cfd = driver.find_element(By.XPATH, "/html/body/table[3]/tbody/tr/td[3]/table/tbody/tr/td/table/tbody/tr[2]/td/b").text
+        results["info"][f"case_{i - 2}"]['CaseFileDate'] = cfd[-4:] + cfd[0:2] + cfd[3:5]
         results["info"][f"case_{i - 2}"]['CaseNumber'] = driver.find_element(By.XPATH, "/html/body/div[2]/span").text
         results["info"][f"case_{i - 2}"]['Category'] = "CRIMINAL"
         results["info"][f"case_{i - 2}"]['CourtJurisdiction'] = 'FORT BEND'
+        results["info"][f"case_{i - 2}"]['CourtName'] = driver.find_element(By.XPATH, "/html/body/table[3]/tbody/tr/td[3]/table/tbody/tr/td/table/tbody/tr[3]/td/b").text
+        # charge(s)
+        charge_table = driver.find_element(By.XPATH, "/html/body/table[6]/tbody")
+        disposition_table = driver.find_element(By.XPATH, "/html/body/table[8]/tbody")
+        ct_trs = charge_table.find_elements(By.TAG_NAME, "tr")
+        dt_trs = disposition_table.find_elements(By.TAG_NAME, "tr")
+        counter = 0
+        # Fixme: try this for "Lee, Derrick Tinnell" who has 2 charges; also fix inconsistencies in XPATH
+        for j in range(1, len(ct_trs), 2):
+            counter += 1
+            results["info"][f"case_{i - 2}"][f"charge_{counter}"] = {}
+            tds = ct_trs[j].find_elements(By.TAG_NAME, "td")
+            results["info"][f"case_{i - 2}"][f"charge_{counter}"]["ChargeFileDate"] = tds[5].text[-4:] + tds[5].text[0:2] + tds[5].text[3:5]
+            results["info"][f"case_{i - 2}"][f"charge_{counter}"]["Comment"] = driver.find_element(By.XPATH, f"/html/body/table[{6+j}]/tbody/tr[2]/td/table/tbody/tr[5]/td/table/tbody/tr/td[2]").text
+            ad = driver.find_element(By.XPATH, f"/html/body/table[{6+j}]/tbody/tr[2]/td/table/tbody/tr[4]/td/table/tbody/tr/td[1]").text
+            results["info"][f"case_{i - 2}"][f"charge_{counter}"]["ArrestDate"] = ad[-4:] + ad[0:2] + ad[3:5]
+            results["info"][f"case_{i - 2}"][f"charge_{counter}"]["CountyOrJurisdiction"] = "FORT BEND"
+
+            for k in range(len(dt_trs)):
+                if "Judgment" in dt_trs[k].text:
+                    d = dt_trs[k].find_element(By.XPATH, "./td[3]/div/div/div/div[1]")
+                    dd = dt_trs[k].find_element(By.TAG_NAME, "th")
+                    results["info"][f"case_{i - 2}"][f"charge_{counter}"]["Disposition"] = d.text
+                    results["info"][f"case_{i - 2}"][f"charge_{counter}"]["DispositionDate"] = dd.text[-4:] + dd.text[0:2] + dd.text[3:5]
+                elif "Committed" in dt_trs[k].text:
+                    sentence = dt_trs[k].find_elements(By.TAG_NAME, "span")
+                    results["info"][f"case_{i - 2}"][f"charge_{counter}"]["Sentence"] = sentence.text()
+                else:
+                    results["info"][f"case_{i - 2}"][f"charge_{counter}"]["Sentence"] = ""
+
+            results["info"][f"case_{i - 2}"][f"charge_{counter}"]["OffenseCode"] = driver.find_element(By.XPATH, f"/html/body/table[6]/tbody/tr[{1+j}]/td[4]").text + " - " + \
+                                                                                   driver.find_element(By.XPATH, f"/html/body/table[6]/tbody/tr[{1+j}]/td[2]").text
+            results["info"][f"case_{i - 2}"][f"charge_{counter}"]["OffenseDescription"] = driver.find_element(By.XPATH, f"/html/body/table[6]/tbody/tr[{1+j}]/td[2]").text
+            results["info"][f"case_{i - 2}"][f"charge_{counter}"]["Severity"] = driver.find_element(By.XPATH, f"/html/body/table[6]/tbody/tr[{1+j}]/td[5]").text
+            results["info"][f"case_{i - 2}"][f"charge_{counter}"]["Statute"] = driver.find_element(By.XPATH, f"/html/body/table[6]/tbody/tr[{1+j}]/td[4]").text
+        results["info"]
+
 
         # time.sleep(2)
 
             # driver.back()
 
-#  webDriver.findElement(By.xpath("//a[@href='/docs/configuration']")).click();
+# driver.quit()
